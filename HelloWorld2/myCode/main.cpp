b057aaada4b089af92878db140b1ed927aec18df
R27188 <R27188@HP10039669.u-dom1.u-ssi.net>
2016-02-01 16:45:50 +0100

    Hello E.ON added

---
+++ HelloWorld2/myCode/main.cpp
@@ -0,0 +1,29 @@
+// GIT-Labor
+// main.h
+
+////////////////////////////////////////////////////////////////////////////////
+// Header-Dateien
+#include <iostream>		// Header für die Standard-IO-Objekte (z.B. cout, cin)
+#include <stdlib.h>
+// TODO: Fügen Sie hier weitere benötigte Header-Dateien der
+// Standard-Bibliothek ein z.B.
+// #include <string>
+
+using namespace std;	// Erspart den scope vor Objekte der
+						// C++-Standard-Bibliothek zu schreiben
+						// z.B. statt "std::cout" kann man "cout" schreiben
+
+// Inkludieren Sie hier die Header-Files Ihrer Klassen, z.B.
+// #include "CFraction.h"
+
+
+// Hauptprogramm
+// Dient als Testrahmen, von hier aus werden die Klassen aufgerufen
+int main (void)
+{
+    // TODO: Fügen Sie ab hier Ihren Programmcode ein
+	cout << "HelloWorld2 gestarted." << endl << endl;
+	cout << "Hello E.ON" << endl << endl;
+	return 0;
+
+}
